c102ce8219321d78731cd0459472549f4838fa05
Trevor Van Rys <trevorvanrys@Trevors-MacBook-Pro-2.local>
2018-01-28 15:18:53 -0500

    updating views, functional chat

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 // initialize our app and tell it to use some plugins from the modules folder
-var express = require('express');
-var app 		= express();
-var http 		= require('http').Server(app);
-var io 			= require('socket.io')(http);
+const express = require('express');
+const app = express();
+//var http = require('http').Server(app);
+const io = require('socket.io')(); //(http)
 
 // some config stuff
-// tell our app to serve css files from that folder
+const PORT = process.env.port || 3000;
+
+// tell our app to serve static files from the public folder
 app.use(express.static('public'));
 
 app.use(require('./routes/index'));
 app.use(require('./routes/contact'));
 app.use(require('./routes/users'));
+
+// tell the app to be served up at this port (same as WAMP or MAMP, just a different port)
+const server = app.listen(3000, function() {
+	console.log('listening on localhost:3000');
+});
+
+io.attach(server);
 
 // plug in socket.io
 io.on('connection', function(socket) {
@@ -31,9 +40,4 @@ io.on('connection', function(socket) {
 		msg = `${socket.id} has left the building!`;
 		io.emit('disconnect message', msg);
 	});
-});
-
-// tell the app to be served up at this port (same as WAMP or MAMP, just a different port)
-http.listen(3000, function() {
-	console.log('listening on localhost:3000');
 });
