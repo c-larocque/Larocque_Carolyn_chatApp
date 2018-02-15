fed27d25482f7283165c3b1f093adb15cfa06228
Carolyn Larocque <clarocque1@live.ca>
2018-02-15 08:50:44 -0500

    Adding styling and trying to add functionality for client counting

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 (function() {
 	const socket = io(); // a constant is a variable that should never change (remains constant)
-	//const nName = "julio";
+	//const socket = io('/my-namespace');
 
 	let messageList = document.querySelector('ul'),
 			chatForm 	= document.querySelector('form'),
@@ -11,6 +11,47 @@
 	function setNickname() {
 		nickName = this.value;
 	}
+
+	// var room = "abc123";
+  //
+	// socket.on('connect', function() {
+	// 	socket.emit('room', room);
+	// });
+
+	socket.on('message', function(data) {
+		console.log('Incoming message:', data);
+	});
+
+	// function setUsername() {
+  //        socket.emit('setUsername', document.getElementById('name').value);
+  //     };
+  //     var user;
+  //     socket.on('userExists', function(data) {
+  //        document.getElementById('error-container').innerHTML = data;
+  //     });
+  //     socket.on('userSet', function(data) {
+  //        user = data.username;
+  //        document.body.innerHTML = '<input type = "text" id = "message">\
+  //        <button type = "button" name = "button" onclick = "sendMessage()">Send</button>\
+  //        <div id = "message-container"></div>';
+  //     });
+
+      function sendMessage() {
+         var msg = document.getElementById('message').value;
+         if(msg) {
+            socket.emit('msg', {message: msg, user: user});
+         }
+      }
+      socket.on('newmsg', function(data) {
+         if(user) {
+            document.getElementById('message-container').innerHTML += '<div><b>' +
+               data.user + '</b>: ' + data.message + '</div>'
+         }
+      })
+
+			// socket.on('typing', function (msg) {
+		  //    io.emit('typing', { 'message': msg.message, 'username': msg.username });
+			// });
 
 	function handleSendMessage(e) {
 		e.preventDefault(); // kill form submit
@@ -33,6 +74,10 @@
 		let newMsg = `<li>${msg}</li>`
 		messageList.innerHTML += newMsg;
 	}
+
+  // socket.on('stats', function(data) {
+  //     console.log('Connected clients:', data.numClients);
+  // });
 
 	nameInput.addEventListener('change', setNickname, false);
 	chatForm.addEventListener('submit', handleSendMessage, false);
